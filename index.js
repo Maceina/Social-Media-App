ee940a443fa01d04f43185e0bcc49792e13069db
Maceina <maceinaaudrius@gmail.com>
2021-02-22 13:51:08 +0200

    your commit message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
-
+// c
 const server = new ApolloServer({
   typeDefs,
   resolvers,
